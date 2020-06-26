299ec81c811aa7176f26c86b48a4f857ab88151f
Zachary Farrell <zfarrell0507@gmail.com>
2020-06-26 17:22:35 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -3,6 +3,7 @@ require 'pry'
 def my_all?(collection)
   i = 0
   while i < collection.length
-    i = i + 1   #i += 1 does the same thing. Use this if it's easier for you.
+    yield(collection[i])
+    i = i + 1
   end
 end
